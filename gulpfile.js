330f3ba643e435823c6ee02e4c914c3a5631503b
Paavveel <65166970+Paavveel@users.noreply.github.com>
2020-08-18 23:45:56 +0300

    Полностью доделал Gulp сборку

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
+const rename = require("gulp-rename");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const svgstore = require("gulp-svgstore");
 const sync = require("browser-sync").create();
 const csso = require("gulp-csso");
-const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const svgstore = require("gulp-svgstore");
 const del = require("del");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
 
 // clean Build folder
 
@@ -19,6 +22,25 @@ const clean = () => {
 };
 
 exports.clean = clean;
+
+// Copy fonts, pictures, scripts to Build folder
+
+const copy = () => {
+  return gulp
+    .src(
+      [
+        "source/fonts/**/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/*.ico",
+      ],
+      { base: "source" }
+    )
+
+    .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
 
 // Styles
 
@@ -29,10 +51,11 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
 
@@ -72,37 +95,42 @@ const sprite = () => {
     .src("source/img/**/icon-*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 };
 
 exports.sprite = sprite;
 
-// Copy fonts, pictures, scripts to Build folder
+// Html minify
 
-const copy = () => {
+const html = () => {
   return gulp
-    .src(
-      [
-        "source/fonts/**/*.{woff,woff2}",
-        "source/img/**",
-        "source/js/**",
-        "source/*.ico",
-        "source/*.html",
-      ],
-      { base: "source" }
-    )
-
-    .pipe(gulp.dest("build"));
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest("build/"))
+    .pipe(sync.stream());
 };
 
-exports.copy = copy;
+exports.html = html;
+
+// Js minify
+
+const scripts = () => {
+  return gulp
+    .src("source/js/index.js")
+    .pipe(gulp.dest("build/js"))
+    .pipe(uglify())
+    .pipe(rename("index.min.js"))
+    .pipe(gulp.dest("build/js"));
+};
+
+exports.scripts = scripts;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: "source",
+      baseDir: "build",
     },
     cors: true,
     notify: false,
@@ -116,14 +144,17 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html", gulp.series(html)).on("change", sync.reload);
+  gulp.watch("source/js/*.js", gulp.series(scripts)).on("change", sync.reload);
 };
-
-// npm start - default
-
-exports.default = gulp.series(styles, server, watcher);
 
 // Build
 
-exports.build = gulp.series(clean, styles, images, copy);
+const build = gulp.series(clean, copy, styles, sprite, html, scripts);
+
+exports.build = build;
+
+// npm start - default
+
+exports.default = gulp.series(build, server, watcher);
